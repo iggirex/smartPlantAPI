6583205ac11a95ea33b3592d7efce547ba4d3d41
Iggi <iggirex@gmail.com>
2016-11-29 14:58:01 -0700

    fixing typos on db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
-var ObjectID = mongo.ObjectID;
+var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
 
